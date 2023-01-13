26d818cf4dc7586ccdb505b666945b4809700ca1
Gong Chen <gong@sprig.com>
2023-01-12 22:17:39 -0800

    added workflow to publish to cocoapods, create tag, and release automatically

---
+++ scripts/tag-and-release.sh
@@ -0,0 +1,51 @@
+#!/usr/env/bin bash
+
+set -e
+
+readonly ACTION="$1"
+readonly GH_TOKEN="$2"
+readonly TAG_NAME="$3"
+if [[ -z "$GH_TOKEN" ]] || [[ -z "$ACTION" ]] || [[ -z "$TAG_NAME" ]]; then
+    echo "The action, access token, or tag name was not set"
+    exit 1
+fi
+
+tag() {
+    git tag "$TAG_NAME"
+    git push origin "$TAG_NAME"
+}
+
+generate_release_payload() {
+    cat << EOF
+{
+    "tag_name":"$TAG_NAME",
+    "target_commitish":"main",
+    "name":"$TAG_NAME",
+    "draft":false,
+    "prerelease":false,
+    "generate_release_notes":true
+}
+EOF
+}
+
+release() {
+    curl \
+        -X POST \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: token $GH_TOKEN" \
+        -d "$(generate_release_payload)" \
+        https://api.github.com/repos/UserLeap/userleap-ios-sdk-releases/releases
+}
+
+main() {
+    if [[ "$1" = "tag" ]]; then
+        tag
+    elif [[ "$1" = "release" ]]; then
+        release
+    else
+        echo "Error: Action $1 not recognized"
+        exit 1
+    fi
+    exit 0
+}
+main "$ACTION"
